8d85e72289331868b1a327d53ff047dba181ee32
necroogre <necroogre@163.com>
2018-06-08 18:03:43 +0800

    draw my hero

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>花Q！</h1>  
-    <img src="../assets/faQ.gif">  
-  </div>
+<div class="hello">
+  <canvas id="myCanvas" ref="canvas" width="1000" height="600"></canvas>
+  <img id="hero" ref="hero" src="../assets/sp.png" alt="hero">
+  <div></div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ctx: '',
+      cX: 0,
+      cY: 0,
+      cWidth: 20,
+      cHeight: 20,
+    }
+  },
+  mounted() {
+    var canvas = this.$refs.canvas;
+    this.ctx = canvas.getContext("2d");
+    this.drawHero()
+  },
+  methods: {
+    drawHero: function() {
+      var img = this.$refs.hero;
+      this.ctx.drawImage(img, 28, 21, 20, 20, this.cX, this.cY, this.cWidth, this.cHeight);
     }
   }
 }
@@ -18,21 +34,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+#myCanvas {
+  border: 1px solid black
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+#hero {
+  width: 72px;
+  height: 80px;
 }
 </style>
